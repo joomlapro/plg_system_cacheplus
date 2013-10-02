77f8346225516301341f09048ada872e47b6e0ff
Bruno Batista <bruno@brunobatista.eti.br>
2013-10-02 09:48:02 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Cache Plus for Joomla! 3.x
+==========================
+
+Combines, minifies, and caches JavaScript and CSS files on demand to speed up page loads.
+
+Using [minify](http://code.google.com/p/minify/).
+
+## Authors
+
+* Bruno Batista
+
+## License
+
+Licensed under the terms of the MIT license.
+
+## Bugs/Requests
+
+* You can [report a bug or request a feature here](http://github.com/joomlapro/plg_system_cacheplus/issues)
---
+++ cacheplus.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.Cacheplus
+ *
+ * @author      Bruno Batista <bruno.batista@ctis.com.br>
+ * @copyright   Copyright (C) 2013 CTIS IT Services. All rights reserved.
+ * @license     Commercial License
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+/**
+ * Joomla Cacheplus plugin.
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  System.Cacheplus
+ * @author      Bruno Batista <bruno.batista@ctis.com.br>
+ * @since       3.2
+ */
+class PlgSystemCacheplus extends JPlugin
+{
+	/**
+	 * After the framework has rendered the application.
+	 *
+	 * @return  void
+	 *
+	 * @since   3.2
+	 */
+	public function onAfterRender()
+	{
+		// Get the application.
+		$app = JFactory::getApplication('site');
+
+		// Check that we are in the site application.
+		if ($app->isAdmin())
+		{
+			return true;
+		}
+
+		// Register dependent classes.
+		JLoader::register('Minify_HTML', __DIR__ . '/libraries/Minify/HTML.php');
+		JLoader::register('Minify_CSS', __DIR__ . '/libraries/Minify/CSS.php');
+		JLoader::register('JSMin', __DIR__ . '/libraries/JSMin.php');
+
+		// Set the options.
+		$options = array();
+		$options['cssMinifier'] = array('Minify_CSS', 'minify');
+		$options['jsMinifier']  = array('JSMin', 'minify');
+
+		// Process the content.
+		$content = JResponse::getBody();
+		$start   = strlen($content);
+		$content = Minify_HTML::minify($content, $options);
+		$end     = strlen($content);
+		$savings = round((($start - $end) / $start * 100), 3);
+
+		// Set the body content.
+		JResponse::setBody($content);
+
+		// Show the process compression.
+		JResponse::appendBody("<!-- Uncompressed size: $start bytes; Compressed size: $end bytes; $savings% savings. -->");
+
+		return true;
+	}
+}
